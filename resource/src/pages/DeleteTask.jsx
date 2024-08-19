dc7a73567fe7b3e7adaa6e15e8a9f6695a020337
priyarshini <dhivyalakshmi@Dhivyas-MacBook-Pro.local>
2024-08-19 13:58:59 +0530

    initial commit

--- resource/src/pages/DeleteTask.jsx
+++ resource/src/pages/DeleteTask.jsx
@@ -2,14 +2,11 @@ import { toast } from 'react-toastify';
 import customFetch from '../utils/customFetch';
 import { redirect } from 'react-router-dom';
 
-export const action =
-  (queryClient) =>
-  async ({ params }) => {
+export const action = async ({ params }) => {
     try {
-      await customFetch.delete(`/jobs/${params.id}`);
-      queryClient.invalidateQueries(['jobs']);
+      await customFetch.delete(`/tasks/${params.id}`);
 
-      toast.success('Job deleted successfully');
+      toast.success('Task deleted successfully');
     } catch (error) {
       toast.error(error?.response?.data?.msg);
     }
